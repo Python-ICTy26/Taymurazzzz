38afd8226bda4be2b68b5683df8685c290188d69
Taymurazzzz <112979718+Taymurazzzz@users.noreply.github.com>
2022-12-21 02:22:05 +0300

    Add files via upload

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -1,4 +1,5 @@
 import typing as tp
+
 from vkapi.friends import get_friends
 
 
--- homework05/research/network.py
+++ homework05/research/network.py
@@ -1,5 +1,6 @@
 import typing as tp
 from collections import defaultdict
+
 import community as community_louvain
 import matplotlib.pyplot as plt
 import networkx as nx
