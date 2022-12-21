4f57c59075a2eec24b0fd77d176db0498869876e
Taymurazzzz <112979718+Taymurazzzz@users.noreply.github.com>
2022-12-21 13:50:04 +0300

    Add files via upload

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -14,7 +14,7 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     """
     age = 0
     friend_ages = []
-    f = get_friends(user_id)
+    f = get_friends(user_id).items
     for i in range(0, len(f)):
         try:
             friend_ages.append(f[i]["bdate"].split(".")[2])
